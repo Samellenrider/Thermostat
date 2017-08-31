a5b0cfbf610978ce351e4b5feefb2c7f1bc3bd92
Sam Ellenrieder <sam.ell@web.de>
2017-08-31 15:54:16 +0100

    Termostat-challange, temp low, first steps

--- jasmine-standalone-2.8.0/spec/thermostatSpec.js
+++ jasmine-standalone-2.8.0/spec/thermostatSpec.js
@@ -6,7 +6,6 @@ describe("Thermostat", function() {
     thermostat = new Thermostat();
   });
 
-
   describe("temperature is 20 by default", function() {
     it('20 degrees', function() {
       expect(thermostat.temperature).toEqual(20);
@@ -17,6 +16,19 @@ describe("Thermostat", function() {
     it('raises temperature by 5 degrees', function() {
       thermostat.up(5)
       expect(thermostat.temperature).toEqual(25);
+    });
+  });
+
+  describe("temperature decrease with down", function() {
+    it("lowers temperature by 5 degrees", function() {
+      thermostat.down(5)
+      expect(thermostat.temperature).toEqual(15);
+    });
+  });
+
+  describe("temperature low is 10 degrees", function() {
+    it("error lower than 10 degrees", function() {
+      expect(thermostat.down(11)).toEqual("You can't go below 10 degrees!");
     });
   });
 });
--- jasmine-standalone-2.8.0/src/thermostat.js
+++ jasmine-standalone-2.8.0/src/thermostat.js
@@ -5,3 +5,17 @@
  Thermostat.prototype.up = function(value) {
    this.temperature += value;
  };
+
+ Thermostat.prototype.down = function(value) {
+   this.temperature -= value;
+ };
+
+ if (temperature <== 10) {
+   console.log("You can't go below 10 degrees!")
+ }
+
+
+
+
+
+   var error = "You can't go below 10 degrees!"
