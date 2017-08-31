b79ba428d0592c7f71242f226ca259f6660c0a7d
Nick Otter <nicholas.a.otter@gmail.com>
2017-08-31 14:54:26 +0100

    Thermostat-challenge, added up function and tests

--- jasmine-standalone-2.8.0/spec/thermostatSpec.js
+++ jasmine-standalone-2.8.0/spec/thermostatSpec.js
@@ -12,4 +12,11 @@ describe("Thermostat", function() {
       expect(thermostat.temperature).toEqual(20);
     });
   });
+
+  describe("temperature increase with up", function() {
+    it('raises temperature by 5 degrees', function() {
+      thermostat.up(5)
+      expect(thermostat.temperature).toEqual(25);
+    });
+  });
 });
