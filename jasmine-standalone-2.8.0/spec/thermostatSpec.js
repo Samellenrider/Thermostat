edb9b063184014ba689e2613fcb8a693a96abfc7
Sam Ellenrieder <sam.ell@web.de>
2017-08-31 16:17:31 +0100

    Thermostat-challenge, throw error

--- jasmine-standalone-2.8.0/spec/thermostatSpec.js
+++ jasmine-standalone-2.8.0/spec/thermostatSpec.js
@@ -12,23 +12,21 @@ describe("Thermostat", function() {
     });
   });
 
-  describe("temperature increase with up", function() {
+
     it('raises temperature by 5 degrees', function() {
       thermostat.up(5)
       expect(thermostat.temperature).toEqual(25);
     });
-  });
 
-  describe("temperature decrease with down", function() {
+
+
     it("lowers temperature by 5 degrees", function() {
       thermostat.down(5)
       expect(thermostat.temperature).toEqual(15);
     });
-  });
+  
 
-  describe("temperature low is 10 degrees", function() {
-    it("error lower than 10 degrees", function() {
-      expect(thermostat.down(11)).toEqual("You can't go below 10 degrees!");
-    });
+  it('temperature cant be decreased lower than 10', function() {
+    expect(function() {thermostat.down(11)}).toThrow('cannot decrease temperature lower than min');
   });
 });
--- jasmine-standalone-2.8.0/src/thermostat.js
+++ jasmine-standalone-2.8.0/src/thermostat.js
@@ -1,21 +1,15 @@
- function Thermostat(){
+function Thermostat(){
     this.temperature = 20;
- };
+  };
 
- Thermostat.prototype.up = function(value) {
+  Thermostat.prototype.up = function(value) {
    this.temperature += value;
- };
+  };
 
- Thermostat.prototype.down = function(value) {
-   this.temperature -= value;
- };
-
- if (temperature <== 10) {
-   console.log("You can't go below 10 degrees!")
- }
-
-
-
-
-
-   var error = "You can't go below 10 degrees!"
+  Thermostat.prototype.down = function (value){
+    if (this.temperature - value < 10) {
+       throw 'cannot decrease temperature lower than min';
+    } else {
+      this.temperature -= value;
+    };
+   };
